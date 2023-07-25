55765dffc170af8ae67b49f4cc9c2b3c3624602f
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-25 22:12:39 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "api_key";
 const url = `https://api.thecatapi.com/v1/breeds`;
 const api_key = "live_n1s7YtWElldkVXS9keSKQYZWhS34TNr45yJqh0E7gf9C9Nv0bvhGcHmFsOwjx9F3"
-
-fetch(url,{headers: {
+function fetchBreeds() {
+ return fetch(url,{headers: {
       'x-api-key': api_key
     }})
- .then((response) => {
-   return response.json();
- })
- .then((data) => {
+  .then((response) => {
+    if (!response) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  }
+ )
+}
+
+ fetchBreeds().then((data) => {
    
    //filter to only include those with an `image` object
-     console.log(data)})
+     console.log(data)}).catch(err => console.log(err))
