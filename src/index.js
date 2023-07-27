7f1171753065a127166c884cdf2107508ef58409
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-27 16:00:00 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import axios from "axios";
+//import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import './styles.css';
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
 
-axios.defaults.headers.common["x-api-key"] = "api_key";
-const url = `https://api.thecatapi.com/v1/breeds`;
-const api_key = "live_n1s7YtWElldkVXS9keSKQYZWhS34TNr45yJqh0E7gf9C9Nv0bvhGcHmFsOwjx9F3"
-function fetchBreeds() {
- return fetch(url,{headers: {
-      'x-api-key': api_key
-    }})
-  .then((response) => {
-    if (!response) {
-      throw new Error(response.statusText)
-    }
-    return response.json()
-  }
- )
-}
+ new SlimSelect({
+  select: '.breed-select'
+ })
 
- fetchBreeds().then((data) => {
-   
-   //filter to only include those with an `image` object
-     console.log(data)}).catch(err => console.log(err))
+ 
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,3 +1,26 @@
-// function fetchBreeds() {
-    
-// }
+axios.defaults.headers.common["x-api-key"] = "api_key";
+const url = 'https://api.thecatapi.com/v1/breeds';
+axios.defaults.headers.common["x-api-key"] = "live_n1s7YtWElldkVXS9keSKQYZWhS34TNr45yJqh0E7gf9C9Nv0bvhGcHmFsOwjx9F3";
+function fetchBreeds() {
+    return axios.get(`${url}`)
+  .then((response) => {
+    if (!response) {
+      throw new Error(response.statusText)
+    }
+ return response.json()
+  }
+ )
+}
+fetchBreeds().then((data) => console.log(data))
+function fetchCatByBreed(breedId) {
+return axios.get(`${url}/images/search?breed_ids=${breedId}`)
+  .then((response) => {
+    if (!response) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  }
+ )
+}
+
+ 
