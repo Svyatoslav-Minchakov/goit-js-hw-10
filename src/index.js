e079982d3e6eb0a7dc7e8ad1dc000d799e031f1c
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-27 17:00:44 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 import './styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
 
- new SlimSelect({
-  select: '.breed-select'
- })
+//  new SlimSelect({
+//   select: '.breed-select'
+//  })
 
- 
+
+const selector = document.querySelector('.breed-select');
+const divCatInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+
+
+fetchBreeds().then(console.log) 
+let arrBreedsId = [];
+fetchBreeds()
+.then(data => {
+    data.forEach(element => {
+        arrBreedsId.push({text: element.name, value: element.id});
+    });
+    new SlimSelect({
+        select: selector,
+        data: arrBreedsId
+    });
+}).catch(onFetchError);
+console.log(arrBreedsId)
+    
+function onFetchError(error) {
+    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+        position: 'center-center',
+        timeout: 5000,
+        width: '400px',
+        fontSize: '24px'
+    });
+};
+   
